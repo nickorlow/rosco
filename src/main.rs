f71758a9edafead930510e2e7dfb561b6bdb41a7
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-10-27 10:37:04 -0500

    output to vga

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static BOOT_MSG: &[u8] = b"CONTROL TRANSFERRED TO ROSCO...";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // write boot message to VGA buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in BOOT_MSG.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
