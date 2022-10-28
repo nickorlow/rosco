7d65c1cf77d46af531ee08b858f19e8c61c759ea
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-10-28 00:29:30 -0500

    vga text functionality (no scrolling yet) and a (cheesy and poorly made) kernel panic screen

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,50 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
-static BOOT_MSG: &[u8] = b"CONTROL TRANSFERRED TO ROSCO...";
+mod vga_txt;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // write boot message to VGA buffer
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in BOOT_MSG.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    println!("Hello, World!");
+    set_color!(0xa);
+    println!("Helno, World!");
+    
+    panic!("OS not built yet!");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    print_panic(_info);
     loop {}
+}
+
+fn print_panic(info: &PanicInfo) {
+    clrscr!();
+    set_color!(0x1f);
+    print!("                                                                                ");
+    print!("                                                                                ");
+    
+    print!  ("                                ");
+    set_color!(0x71);
+    print!  (" Kernel Panic! ");
+    set_color!(0x1f);
+    print!  ("                                 ");
+    
+    print!("                                                                                ");
+    print!("                                                                                ");
+    print!("                  ROSCO has encountered a fatal error. Error:                   ");
+    print!("             {}                     " , info);
+    print!("                                                                                ");
+    print!("           This is due to a fault in the OS. Please report it to                ");
+    print!("                  https://github.com/nickorlow/rosco                            ");
+    for n in 10..20 {
+        print!("                                                                                ");
+    }
+    print!("                      THERE IS NO CRYING IN PINTOS!                             ");
+    print!("                                                                                ");
+    print!("                                                                                ");
+    print!("                                                                                ");
+    print!("                                                                                ");
 }
